c71d4664c27a3d72a8cf9eccb09614d3607c29c1
luisarmada <123182885+luisarmada@users.noreply.github.com>
2025-05-14 06:05:10 +0100

    odm level out

--- objects/characters/player/animation_controller.gd
+++ objects/characters/player/animation_controller.gd
@@ -27,6 +27,11 @@ var lookat_cutoff: float = 0.8 # if greater, deactivate look at
 var arm_ik_target: float = 0.0
 var arm_ik_value: float = 0.0
 
+@export var arm_modifiers: Dictionary
+
+@export var odm_left: Node3D
+@export var odm_right: Node3D
+
 func _ready() -> void:
 	anim_tree.callback_mode_process = AnimationMixer.ANIMATION_CALLBACK_MODE_PROCESS_MANUAL
 
@@ -41,8 +46,6 @@ func _physics_process(delta: float) -> void:
 	on_ground_blend_target = 1 if character.is_on_floor() else 0
 	on_ground_blend = lerp(on_ground_blend, on_ground_blend_target, 10 * delta)
 	anim_tree["parameters/Katana/on_ground_blend/blend_amount"] = on_ground_blend
-	
-	
 	
 	# Head look at location logic
 	if on_ground_blend_target != 0:
@@ -60,8 +63,6 @@ func _physics_process(delta: float) -> void:
 			var forward_vector = -character_mesh.global_transform.basis.z.normalized()
 			var dot_product = forward_vector.dot(lookat_dir)
 			
-			
-			print(dot_product)
 			if dot_product > 0.4 and dot_product < 1.0:
 				# Look at camera
 				update_look_at_target(camera_target_node)
@@ -83,20 +84,29 @@ func update_look_at_target(target: Node3D, allow_vertical: bool = true, smoothin
 	look_at_smooth_follower.update_target_node(look_at_target, smoothing)
 
 func _on_set_movement_state(_movement_state: MovementState) -> void:
-		if _movement_state.movement_speed > 0 and on_ground_blend_target == 1:
-			anim_tree["parameters/Katana/sprint_end/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FADE_OUT
+	if _movement_state.movement_speed > 0 and on_ground_blend_target == 1:
+		anim_tree["parameters/Katana/sprint_end/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FADE_OUT
+
+	if _movement_state.max_camera_pitch < 20.0:
+		lookat_camera_modifier.use_secondary_rotation = false
+	else:
+		lookat_camera_modifier.use_secondary_rotation = true
 	
-		if _movement_state.max_camera_pitch < 20.0:
-			lookat_camera_modifier.use_secondary_rotation = false
-		else:
-			lookat_camera_modifier.use_secondary_rotation = true
+	if tween:
+		tween.kill()
 		
-		if tween:
-			tween.kill()
-			
-		tween = create_tween()
-		tween.tween_property(anim_tree, "parameters/Katana/ground_movement/blend_position", _movement_state.id, 0.4)
-		tween.parallel().tween_property(anim_tree, "parameters/Katana/movement_anim_speed/scale", _movement_state.animation_speed, 0.7)
+	var should_ik_arm: bool = _movement_state.movement_speed < 20
+	
+	tween = create_tween()
+	tween.tween_property(anim_tree, "parameters/Katana/ground_movement/blend_position", _movement_state.id, 0.4)
+	tween.parallel().tween_property(anim_tree, "parameters/Katana/movement_anim_speed/scale", _movement_state.animation_speed, 0.7)
+	
+	tween.parallel().tween_property(odm_left, "rotation_degrees", Vector3(2.0 if should_ik_arm else -30.0, odm_left.rotation_degrees.y, odm_left.rotation_degrees.z), 0.25).set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_property(odm_right, "rotation_degrees", Vector3(2.0 if should_ik_arm else 30.0, odm_right.rotation_degrees.y, odm_right.rotation_degrees.z), 0.25).set_ease(Tween.EASE_OUT)
+	
+	for i in arm_modifiers.keys():
+		tween.parallel().tween_property(get_node(i), "influence", arm_modifiers[i] if should_ik_arm else 0, 0.2).set_ease(Tween.EASE_OUT)
+	
 
 func _on_jump(_jump_state: JumpState):
 	anim_tree["parameters/Katana/ground_jump/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE
